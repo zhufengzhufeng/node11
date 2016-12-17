3d408983b52d3d35bc941602cc848435ade4f519
zhufengzhufeng <894918097@qq.com>
2016-12-17 11:11:03 +0800

    3中声明方式

--- 6.buffer/1.buffer.js
+++ 6.buffer/1.buffer.js
@@ -11,7 +11,7 @@ for(var i = 1; i<=8;i++){
 console.log(sum); //255
 //buffer中由好多字节组成类似于数组的东西，每个字节最大是多少
 
-//1.声明buffer 一旦声明就需要给一个固定大小
+//1).声明buffer 一旦声明就需要给一个固定大小
 //1.长度（utf8一个汉字几个字节 3个）
 var buffer = new Buffer(40); //三个字节
 buffer.fill(0);//手动清0
@@ -20,5 +20,18 @@ console.log(buffer);//随机抓过来三个字节的内存，默认为随机的
 var buffer = new Buffer('珠峰','utf8');
 console.log(buffer);
 //3.数组 (大于255 对256取模 小于0加上256)
-var buffer = new Buffer([-2,255]);//不识别都是0
+var arr = [-2,255];
+var buffer = new Buffer(arr);//不识别都是0
 console.log(buffer);
+
+//2) buffer的常用方法
+var buffer = new Buffer(6);
+var str = '珠峰培';
+var str1 = '训';
+//string写入的内容
+//offset写入的偏移量
+//length写入的长度
+//encoding编码格式
+buffer.write(str,0,6,'utf8');
+console.log(buffer.toString());//将buffer转换成字符串
+//先写str1 在写str 输出字符珠峰培训
